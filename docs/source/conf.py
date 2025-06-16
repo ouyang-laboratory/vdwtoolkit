822f81e67b9a92ab0e32fe75386fac3575b0d5c4
bhk <b13548849360@gmail.com>
2025-06-16 08:59:51 +0800

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,18 +1,17 @@
-# Configuration file for the Sphinx documentation builder.
-
+# -- Path setup --------------------------------------------------------------
 import os
 import sys
 
-# -- Path setup --------------------------------------------------------------
-# Add project root (two levels up) to Python path, so Sphinx can import vdwtoolkit
-sys.path.insert(0, os.path.abspath(os.path.join('..', '..')))
+# conf.py 的目录
+conf_dir = os.path.dirname(__file__)
+# 项目根目录（docs/ 上一级）
+project_root = os.path.abspath(os.path.join(conf_dir, '..', '..'))
+# 将项目根目录插入到 sys.path
+sys.path.insert(0, project_root)
 
 # -- Project information -----------------------------------------------------
-project = 'vdwtoolkit'
-copyright = '2025, Hekai Bu'
+project = 'vdW-Toolkit'
 author = 'Hekai Bu'
-
-# The full version, including alpha/beta/rc tags
 release = '0.1'
 version = '0.1.0'
 
@@ -22,30 +21,24 @@ extensions = [
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
     'sphinx.ext.viewcode',
+    'sphinx.ext.intersphinx',
     'sphinx_rtd_theme',
 ]
 
-# Generate autosummary pages automatically
 autosummary_generate = True
 
-# Intersphinx mapping for cross-referencing other projects
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
     'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
 }
 intersphinx_disabled_domains = ['std']
 
-# Paths that contain templates, relative to this directory
 templates_path = ['_templates']
-
-# List of patterns, relative to source directory, to ignore when looking for source files
 exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
 import sphinx_rtd_theme
-
 html_theme = 'sphinx_rtd_theme'
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_title = f"{project} v{release} Documentation"
